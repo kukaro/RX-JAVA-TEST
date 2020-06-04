52bb19e6d1ebd028d01071c8084e7209b58d8955
kukaro <justkukaro@naver.com>
2020-06-05 03:05:30 +0900

    add

---
+++ FIRST-PROJECT/src/main/java/RXJAVA2020/Chapter2/P62_Single_just.java
@@ -0,0 +1,16 @@
+package RXJAVA2020.Chapter2;
+
+import io.reactivex.Single;
+
+/*
+
+ */
+public class P62_Single_just {
+    private static Logger log = Logger.getInstance();
+
+    public static void main(String[] args) {
+        Single<String> source = Single.just("Hello Single");
+        source.subscribe(log::info);
+    }
+}
+

6510ca870d6f8589747f93c53a57c7cb7df9a3ca
kukaro <justkukaro@naver.com>
2020-06-05 03:52:50 +0900

    add single

--- FIRST-PROJECT/src/main/java/RXJAVA2020/Chapter2/P62_Single_just.java
+++ FIRST-PROJECT/src/main/java/RXJAVA2020/Chapter2/P62_Single_just.java
@@ -3,7 +3,7 @@ package RXJAVA2020.Chapter2;
 import io.reactivex.Single;
 
 /*
- * 
+ * Single은 Observable중에서 하나만 배포하는 observer다.
  */
 public class P62_Single_just {
     private static Logger log = Logger.getInstance();
